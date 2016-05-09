8893d4efc813c1c6213aa783aeceda2a0906ff49
MarcelHimmelreich <marcel.himmelreich@uni-weimar.de>
2016-05-09 20:32:12 +0200

    Update circle.hpp

--- source/circle.hpp
+++ source/circle.hpp
@@ -10,16 +10,16 @@ class Circle
 public:
 Circle();
 Circle(float rad, Vec2 v);
-Circle(float rad, Vec2 v,color farb);
+Circle(float rad, Vec2 v,color  farb);
 
-void draw(Window& win);
-void draw(Window& win,color c);
+void draw(Window& const& win);
+void draw(Window& const&win,color c);
 void set_radius(float r);      
 float get_radius();            
 float get_area();           
 float get_circum();
 float get_diameter();  
-bool isInside(Vec2 v);
+bool isInside(Vec2 const&v);
 
 void fill(Window& win);
 void fill(Window& win,color c); 
